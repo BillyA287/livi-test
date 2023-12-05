67b5e10034fddafd99fbc687183a3c08fc1a75e1
Billy <atimb287@gmail.com>
2023-12-05 00:07:51 +0000

    fixed navigation and score need to add tests + styling

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,36 +1,60 @@
 import React from 'react';
 import './Card.css';
 
-const Card = ({ question, onAnswer, finalMessage, onReset, onBack, onNext }) => {
-  // destructure props from question
+const Card = ({ question, onAnswer, finalMessage, onReset, onBack, onNext, currentQuestionPosition, isOptionSelected }) => {
   const { question_text, answers } = question;
-  
   const outcomeId = finalMessage?.id.replace(/_/g, ' ');
-  
+  const showBookingButton = finalMessage?.show_booking_button || false;
 
+
+  console.log("Final Message:", finalMessage); 
+  console.log("isOptionSelected:", isOptionSelected);
+  console.log("!question.next:", !question.next);
+  console.log("currentQuestionPosition:", currentQuestionPosition);
+  console.log("Array.isArray(question.next):", Array.isArray(question.next));
+  console.log("question.next.length - 1:", Array.isArray(question.next) ? question.next.length - 1 : 0);
+  
   return (
     <div className="question-card">
+      <button onClick={onBack} disabled={currentQuestionPosition === 0 || isOptionSelected}>Back</button>
       {finalMessage ? (
-        <div><h2>{outcomeId} </h2>
-        <p>{finalMessage.text}</p>
-        <button onClick={onReset}>Reset</button></div> 
-        
+        <div>
+          <h2>{outcomeId} </h2>
+          <p>{finalMessage.text}</p>
+          {showBookingButton && <button>Show Booking Button</button>}
+          <button onClick={onReset}>Reset</button>
+        </div>
       ) : (
         <>
-        <button onClick={onBack}>back</button>
-        <button onClick={onNext}>forward</button>
+          
+  
           <h3>{question_text}</h3>
           <div className="answer-options">
-           
             {answers &&
               answers.map((answer, index) => (
-                <button key={answer.id} onClick={() => onAnswer(index)}>
+                <button
+                  key={answer.id}
+                  onClick={() => {
+                    if (!isOptionSelected) {
+                      onAnswer(index);
+                    }
+                  }}
+                  disabled={isOptionSelected}
+                >
                   {answer.label}
                 </button>
               ))}
           </div>
+          <button
+  onClick={onNext}
+>
+  Next
+</button>
         </>
       )}
+      
+
+
     </div>
   );
 };
--- src/components/Quiz/Quiz.jsx
+++ src/components/Quiz/Quiz.jsx
@@ -1,3 +1,4 @@
+// Quiz.jsx
 import React, { useState } from "react";
 import data from "../../data/form.js";
 import Card from "../Card/Card.jsx";
@@ -5,34 +6,17 @@ import Card from "../Card/Card.jsx";
 const Quiz = () => {
   const [currentQuestionPosition, setCurrentQuestionPosition] = useState(0);
   const [currentScore, setCurrentScore] = useState(0);
+  const [selectedOptionIndex, setSelectedOptionIndex] = useState(null);
 
   const totalScore = (selectedOption) => {
-    // create current question position as we use this to access the score
-    const currentQuestion = data.questions[currentQuestionPosition];
-    // when an answer is clicked we need to check if current question we want to access is in range
-    console.log(selectedOption);
-    if (
-      currentQuestion &&
-      currentQuestion.answers &&
-      currentQuestion.answers.length > selectedOption
-    ) {
-      // if it is we capture the score value of the selected answer
-      let answerScore = currentQuestion.answers[selectedOption].score;
-      // update the state with an updater function to calculate current total score
-      setCurrentScore((prevState) => prevState + answerScore);
+    setSelectedOptionIndex(selectedOption);
 
-      nextUserQuestion();
-      console.log("option value", selectedOption);
-      console.log(currentScore);
-      console.log(currentQuestion);
-    }
   };
 
-  // result message function
+  
+
   const finalMessage = () => {
-    // Check if the user has completed the questionnaire
     if (currentQuestionPosition === data.questions.length - 1) {
-      // Determine the outcome based on the user's score
       let outcome;
 
       if (currentScore < 5) {
@@ -40,22 +24,19 @@ const Quiz = () => {
           (outcome) => outcome.id === "rest_and_come_back_later"
         );
       } else if (currentScore >= 5 && currentScore < 49) {
-        outcome = data.outcomes.find(
-          (outcome) => outcome.id === "see_a_doctor"
-        );
+        outcome = data.outcomes.find((outcome) => outcome.id === "see_a_doctor");
       } else {
         outcome = data.outcomes.find(
           (outcome) => outcome.id === "go_to_emergency_room"
         );
       }
 
-      return outcome; // Return the outcome directly
+      return outcome;
     }
 
     return null;
   };
 
-  // calculate current question position in list and display to user
   const showProgress = () => {
     const position = currentQuestionPosition + 1;
     const totalQuestions = data.questions.length;
@@ -64,23 +45,39 @@ const Quiz = () => {
 
   const handleReset = () => {
     setCurrentQuestionPosition(0);
-    setCurrentScore(0)
-  }
-
-  const nextUserQuestion = () => {
-    setCurrentQuestionPosition((prevPosition) => prevPosition + 1);
+    setCurrentScore(0);
+    setSelectedOptionIndex(null);
   };
 
-  const handleBack =()=>{
-    setCurrentQuestionPosition((prevPosition) => prevPosition - 1);
+  const nextUserQuestion = () => {
+    setSelectedOptionIndex(null);
+    
+  };
 
-  }
+  const handleBack = () => {
+    if (currentQuestionPosition > 0) {
+      setCurrentQuestionPosition((prevPosition) => prevPosition - 1);
+      setSelectedOptionIndex(null);
+    }
+  };
 
   const handleNext = () => {
-    nextUserQuestion()
+    console.log("currentQuestionPosition:", currentQuestionPosition);
+    console.log("data.questions.length:", data.questions.length);
 
-  }
-
+    if (selectedOptionIndex !== null && currentQuestionPosition < data.questions.length - 1) {
+      const currentQuestion = data.questions[currentQuestionPosition];
+      let answerScore = currentQuestion.answers[selectedOptionIndex].score;
+      setCurrentScore((prevState) => prevState + answerScore);
+      setCurrentQuestionPosition((prevPosition) => prevPosition + 1);
+      nextUserQuestion();
+      setSelectedOptionIndex(null); // Reset selected option after updating score
+    } 
+    else if (currentQuestionPosition < data.questions.length - 1) {
+      setCurrentQuestionPosition((prevPosition) => prevPosition + 1);
+    }
+    
+  };
 
   const currentQuestion = () => {
     return data.questions[currentQuestionPosition];
@@ -89,22 +86,22 @@ const Quiz = () => {
   if (currentQuestion()) {
     const position = showProgress();
     const lastMessage = currentQuestionPosition === data.questions.length - 1;
-    const outcome = lastMessage ? finalMessage() : null; // Get the outcome
+    const outcome = lastMessage ? finalMessage() : null;
 
     return (
       <>
         <p>{position}</p>
-        <p>current score:{currentScore}</p>
+        <p>current score: {currentScore}</p>
         <Card
           key={currentQuestion().id}
           question={currentQuestion()}
           onAnswer={(selectedOption) => totalScore(selectedOption)}
-          finalMessage={outcome} // Pass the outcome as a prop
+          finalMessage={outcome}
           currentQuestionPosition={currentQuestionPosition}
-          data={data}
           onReset={handleReset}
           onBack={handleBack}
           onNext={handleNext}
+          selectedOptionIndex={selectedOptionIndex !== null}
         />
       </>
     );
